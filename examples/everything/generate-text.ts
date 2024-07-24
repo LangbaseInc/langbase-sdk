d39f27a3ce402a536f5fe2a4bcb0bba0e48e19aa
Ahmad Awais <me@AhmadAwais.com>
2024-07-24 06:52:39 -0700

    👌 IMPROVE: Example docs

---
+++ examples/everything/generate-text.ts
@@ -0,0 +1,18 @@
+import 'dotenv/config';
+import {Pipe} from 'langbase';
+
+// STREAM: OFF
+console.log('STREAM-OFF: generateText()');
+
+// 1. Initiate the Pipe.
+const pipeStreamOff = new Pipe({
+	apiKey: process.env.PIPE_LESS_WORDY!,
+});
+
+// 3. Generate the text by asking a question.
+const result = await pipeStreamOff.generateText({
+	messages: [{role: 'user', content: 'Who is an AI Engineer?'}],
+});
+
+// 4. Done: You got the generated completion.
+console.log(result.completion);
