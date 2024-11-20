d8656e23556eedc599ff74c1ddb4e80f5e576f43
msaaddev <mrsaadirfan@gmail.com>
2024-11-21 00:42:39 +0100

    📦 NEW: Memory create and list support

--- packages/langbase/src/index.ts
+++ packages/langbase/src/index.ts
@@ -1,5 +1,6 @@
 export {fromReadableStream} from './lib/browser/stream';
 export * from './pipes/pipes';
+export * from './memory/memory';
 export * from '@baseai/core/helpers';
 export type {
 	RunOptions,
