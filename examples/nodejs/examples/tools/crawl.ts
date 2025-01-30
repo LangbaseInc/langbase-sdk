cf6876b383c641ef1ef8a4540b9f5d27ef4cb537
Ashar Irfan <mrasharirfan@gmail.com>
2025-01-31 03:01:43 +0500

    👌 IMPROVE: Crawl example comment

--- examples/nodejs/examples/tools/crawl.ts
+++ examples/nodejs/examples/tools/crawl.ts
@@ -5,6 +5,13 @@ const langbase = new Langbase({
 	apiKey: process.env.LANGBASE_API_KEY!,
 });
 
+/**
+ * Crawls specified URLs using spider.cloud API.
+ *
+ * Get your API key from the following link and set it in .env file.
+ *
+ * @link https://spider.cloud/docs/quickstart
+ */
 async function main() {
 	const results = await langbase.tool.crawl({
 		url: ['https://langbase.com', 'https://langbase.com/about'],
