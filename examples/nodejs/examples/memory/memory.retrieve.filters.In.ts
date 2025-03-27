35e9daacca6d2cfa9169400c31922f2e7859346e
Ankit Kumar <98694380+arre-ankit@users.noreply.github.com>
2025-03-27 05:51:40 +0530

    👌 IMPROVE: memory example with filters (#93)
    * 👌 IMPROVE: memory example with filters
    
    * 🐛 FIX: package.json
    
    * 🐛 FIX: comments for filter examples
    
    * 🐛 FIX: formatting issue
    
    * 🐛 FIX: formatting issue
    
    * 🐛 FIX: package.json

---
+++ examples/nodejs/examples/memory/memory.retrieve.filters.In.ts
@@ -0,0 +1,35 @@
+/**
+ * Basic example to demonstrate how to retrieve memories with filters.
+ * 
+ * - In: This filter is used to retrieve memories that match any of the value/values in the array.
+ * 
+ * In this example, we retrieve memories with the following filters:
+ * - company: Langbase or Google
+ * 
+ * We expect to get all chunks of memory from the Langbase Docs memory that have the company Langbase or Google.
+ * 
+ */
+
+import 'dotenv/config';
+import {Langbase} from 'langbase';
+
+const langbase = new Langbase({
+	apiKey: process.env.LANGBASE_API_KEY!,
+});
+
+async function main() {
+	const response = await langbase.memories.retrieve({
+		memory: [
+		{
+			name: "langbase-docs",
+			filters: ["company", "In", ["Langbase","Google"]],
+		},
+		],
+		query: "What are pipes in Langbase?",
+		topK: 5
+	});
+
+	console.log(response);
+}
+
+main();
