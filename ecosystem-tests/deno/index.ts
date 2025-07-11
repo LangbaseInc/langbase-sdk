d7582354464bda953c7d4fff5b6a97869f5b0175
Ahmad Bilal <ahmadbilaldev@gmail.com>
2025-07-11 22:16:22 +0500

    👌 IMPROVE: Deno

--- ecosystem-tests/deno/index.ts
+++ ecosystem-tests/deno/index.ts
@@ -1,3 +1,7 @@
+// If you are using the Deno VSCode extension or Deno CLI for type checking, the following line enables Deno global types:
+// /// <reference types="deno.ns" />
+// If you see 'Cannot find type definition file for deno.ns', you can safely remove the line above unless you are using Deno tooling.
+// Deno will run this file fine without it.
 // @deno-types="npm:langbase@1.2.0"
 import {Langbase, Workflow} from 'npm:langbase@1.2.0';
 
